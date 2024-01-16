a719c15e3ab61fd8fb98e876db3d1ca6a9d96fe8
R33Bickard <Richard.Bickard@hftm.ch>
2024-01-16 18:02:54 +0100

    JavaFX-Integration #1

--- src/main/java/ch/hftm/quizapp/ErgebnisController.java
+++ src/main/java/ch/hftm/quizapp/ErgebnisController.java
@@ -1,7 +1,5 @@
 package ch.hftm.quizapp;
 
-public class ErgebenisController {
-    public static void main(String[] args) {
-        
-        }
+public class ErgebnisController {
+   
     }
--- src/main/java/ch/hftm/quizapp/QuizApp.java
+++ src/main/java/ch/hftm/quizapp/QuizApp.java
@@ -1,7 +1,28 @@
 package ch.hftm.quizapp;
 
-public class QuizApp {
-    public static void main(String[] args) {
-        System.out.println("Hallo, QuizApp!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class QuizApp extends Application {
+
+@Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(QuizApp.class.getResource("hauptseite.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    
 }
