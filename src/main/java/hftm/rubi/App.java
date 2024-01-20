4e8964e5d04a83ce3fa4fe7589052a4185b438df
R33Bickard <Richard.Bickard@hftm.ch>
2024-01-20 12:23:24 +0100

    JavaFX#4: Visualisierung#1.0

--- src/main/java/hftm/rubi/App.java
+++ src/main/java/hftm/rubi/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("fragen"), 640, 480);
+        scene = new Scene(loadFXML("fragen"));
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
